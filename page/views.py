a3fd2d7537ebe7e8c27327745e37c406e61816d0
Ivan Andreev <ivandeex@gmail.com>
2019-06-25 02:56:39 +0300

    use ListView

--- page/views.py
+++ page/views.py
@@ -1,30 +1,26 @@
 from django.http import Http404
 from .models import Category, Good
-from django.core.paginator import Paginator, InvalidPage
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 
 
-class GoodListView(TemplateView):
+class GoodListView(ListView):
     template_name = 'index.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        try:
+            if 'cat_id' in self.kwargs:
+                self._cat = Category.objects.get(pk=self.kwargs['cat_id'])
+            else:
+                self._cat = Category.objects.first()
+        except Category.DoesNotExist:
+            raise Http404
+        return Good.objects.filter(category=self._cat).order_by('name')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cats'] = Category.objects.all().order_by('name')
-        try:
-            if 'cat_id' in self.kwargs:
-                cat = Category.objects.get(pk=self.kwargs['cat_id'])
-            else:
-                cat = Category.objects.first()
-        except Category.DoesNotExist:
-            raise Http404
-        context['category'] = cat
-        page_no = self.request.GET.get('page')
-        all_goods = Good.objects.filter(category=cat)
-        paginator = Paginator(all_goods, 2)
-        try:
-            context['goods'] = paginator.page(page_no)
-        except InvalidPage:
-            context['goods'] = paginator.page(1)
+        context['category'] = self._cat
         return context
 
 
