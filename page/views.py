53cc4cfb160558fb3fe23bc59d58631c0e0b7d23
Ivan Andreev <ivandeex@gmail.com>
2019-06-24 20:18:06 +0300

    chapter 7

--- page/views.py
+++ page/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpRequest, HttpResponse, Http404
 from .models import Category, Good
-#from django.shortcuts import render
+from django.shortcuts import render
 
 
 def index(req: HttpRequest, cat_id: int = None) -> HttpResponse:
+    cats = Category.objects.all().order_by('name')
     try:
         if cat_id is None:
             cat = Category.objects.first()
@@ -12,10 +13,11 @@ def index(req: HttpRequest, cat_id: int = None) -> HttpResponse:
     except Category.DoesNotExist:
         raise Http404
     goods = Good.objects.filter(category=cat)
-    s = f"Категория: {cat.name} <br><br>"
-    for good in goods:
-        s += f"({good.pk}) {good.name}<br>"
-    return HttpResponse(s)
+    return render(req, 'index.html', {
+        'category': cat,
+        'cats': cats,
+        'goods': goods,
+    })
 
 
 def good(req: HttpRequest, good_id: int) -> HttpResponse:
@@ -23,7 +25,4 @@ def good(req: HttpRequest, good_id: int) -> HttpResponse:
         good = Good.objects.get(pk=good_id)
     except Good.DoesNotExist:
         raise Http404
-    s = f"{good.name}<br><br>{good.category.name}<br><bt>{good.description}"
-    if not good.in_stock:
-        s += "<br><br>нет в наличии!"
-    return HttpResponse(s)
+    return render(req, 'good.html', {'good': good})
